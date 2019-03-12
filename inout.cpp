2015d7adf26c358e08c9b17830dce11db02bbe51
Danil Shapovalov <danil_shapovalov00@mail.ru>
2019-03-12 18:18:50 +0700

    "Cod"

---
+++ inout.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+main()
+{
+    int m, n, r;
+    printf("input parametrs(m,n,r)(cherez probel)");
+    scanf("%d %d %d", &m, &n, &r);
+    printf("circle( %d , %d , %d )", m, n, r);
+    system("PAUSE");
+    return 0;
+}
