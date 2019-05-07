a306bcf878dcaba85fa9cbc11855034040084bfc
Danil <danil_shapovalov00@mail.ru>
2019-05-07 20:37:39 +0700

    Add normal and hard level

--- src/func.h
+++ src/func.h
@@ -3,4 +3,9 @@
 void outOkr();
 double POkr(int r);
 double SOkr(int r);
+void triangle(int xt1, int yt1, int xt2, int yt2, int xt3, int yt3);
+void polygon(
+        int px1, int py1, int px2, int py2, int px3, int py3, int px4, int py4);
+void outTri();
+void outPol();
 #endif
