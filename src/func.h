03d8dc21b36c9c2284f1b2f580fe35befddaa9be
Danil <danil_shapovalov00@mail.ru>
2019-05-08 00:07:29 +0700

    Updated func.o main.c func.h peresechenie was made

--- src/func.h
+++ src/func.h
@@ -1,11 +1,13 @@
 #ifndef FUNC_GUARD
 #define FUNC_GUARD
-void outOkr();
-double POkr(int r);
-double SOkr(int r);
-void triangle(int xt1, int yt1, int xt2, int yt2, int xt3, int yt3);
-void polygon(
+float POkr(int r);
+float SOkr(int r);
+float triangleP(int xt1, int yt1, int xt2, int yt2, int xt3, int yt3);
+float triangleS(int xt1, int yt1, int xt2, int yt2, int xt3, int yt3);
+float polygonS(
         int px1, int py1, int px2, int py2, int px3, int py3, int px4, int py4);
-void outTri();
-void outPol();
+float polygonP(
+        int px1, int py1, int px2, int py2, int px3, int py3, int px4, int py4);
+int outPeres();
+int peres(int x1, int y1, int r1, int x2, int y2, int r2);
 #endif
